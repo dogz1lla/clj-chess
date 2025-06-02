2cbf7f3f18176a83751a43bb7808cf48d3aae655
dogz1lla <dogz1lla@limbo.xyz>
2025-06-02 21:04:34 +0200

    alternative state represenation prototype

--- src/chess/state.clj
+++ src/chess/state.clj
@@ -25,4 +25,34 @@
                             {:piece :pawn :pos [8, 2] :color :black :leap false}]
                     :captured-pieces []
                     :move-history []})
-                            
+
+(defn init-pawn [pos color]
+  {:piece :pawn :pos pos :color color})
+
+(defn init-pawns [{:keys [board] :as state}]
+  (let [white-pos (for [x (range 1 9)] [x 7])
+        black-pos (for [x (range 1 9)] [x 2])
+        add-pawn (fn [color] (fn [s pos] (assoc s pos (init-pawn pos color))))]
+    (reduce
+      (add-pawn :black)
+      (reduce
+        (add-pawn :white)
+        state
+        white-pos)
+      black-pos)))
+
+(defn init-board []
+  (let [squares (for [col (range 1 9) row (range 1 9)] [col row])
+        nils (repeat (count squares) nil)
+        board (zipmap squares nils)
+        board (init-pawns board)]
+    board))
+
+(def alternative-state {:board (init-board)
+                        :turn :white
+                        :history []})
+
+(comment
+  (init-board)
+  (->> (init-board) init-pawns))
+  
