834785733acc667dd2148149bd50cb0bcbf44b62
dogz1lla <dogz1lla@limbo.xyz>
2025-08-24 07:45:41 +0200

    RF: init-state function -- new board structure

--- src/chess/state.clj
+++ src/chess/state.clj
@@ -5,7 +5,10 @@
   (assoc board pos piece))
 
 (defn init-pieces [board {:keys [kind white black]}]
-  (let [add-piece (fn [color] (fn [s [idx pos]] (assoc s pos {:piece kind :pos pos :color color :id (str kind color ":" idx)})))
+  (let [add-piece (fn [color]
+                    (fn [s [idx pos]]
+                      (let [piece-id (str kind color ":" idx)]
+                        (assoc-in s [color piece-id] {:piece kind :pos pos :color color :id piece-id}))))
         enum-white-positions (map vector (range) white)
         enum-black-positions (map vector (range) black)]
       (reduce
@@ -22,8 +25,20 @@
         board (zipmap squares nils)]
     board))
 
+; (defn init-state []
+;   {:board (-> (init-board)
+;               (init-pieces {:kind :king   :white [[5 8]]       :black [[5 1]]})
+;               (init-pieces {:kind :queen  :white [[4 8]]       :black [[4 1]]})
+;               (init-pieces {:kind :rook   :white [[1 8] [8 8]] :black [[1 1] [8 1]]})
+;               (init-pieces {:kind :bishop :white [[3 8] [6 8]] :black [[3 1] [6 1]]})
+;               (init-pieces {:kind :knight :white [[2 8] [7 8]] :black [[2 1] [7 1]]})
+;               (init-pieces {:kind :pawn   :white (for [x (range 1 9)] [x 7]) :black (for [x (range 1 9)] [x 2])}))
+;    :turn :white
+;    :history []
+;    :captured []})
+
 (defn init-state []
-  {:board (-> (init-board)
+  {:board (-> {}
               (init-pieces {:kind :king   :white [[5 8]]       :black [[5 1]]})
               (init-pieces {:kind :queen  :white [[4 8]]       :black [[4 1]]})
               (init-pieces {:kind :rook   :white [[1 8] [8 8]] :black [[1 1] [8 1]]})
@@ -33,10 +48,7 @@
    :turn :white
    :history []
    :captured []})
-
+                 
 (comment
   (init-board)
   (init-state))
-  
-(comment
-  (put-piece-on-board (init-board) (init-piece :pawn [1 1] :white)))
