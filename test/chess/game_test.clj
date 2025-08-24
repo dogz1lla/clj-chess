99145e6db5769be0d6f46bdee3d51d4c810e1678
dogz1lla <dogz1lla@limbo.xyz>
2025-08-24 07:44:39 +0200

    unit tests for game state

--- test/chess/game_test.clj
+++ test/chess/game_test.clj
@@ -17,6 +17,7 @@
                                   (g/calculate-all-attacks)
                                   (g/update-check))]
                      (g/check? state)))))
+
     (t/testing "white king check"
       (t/is (let [piece-1 {:piece :king :color :white :pos [3, 3] :id "king1"}
                   piece-2 {:piece :pawn :color :black :pos [2, 2] :id "pawn1"}
@@ -30,6 +31,22 @@
                 (g/check? state)))))
 
   (t/testing "mate states"
+    (t/testing "white king cornered, no mate"
+      (t/is (not (let [piece-1 {:piece :king :color :white :pos [8, 8] :id "king1"}
+                       piece-2 {:piece :pawn :color :black :pos [8, 7] :id "pawn1"}
+                       piece-3 {:piece :pawn :color :black :pos [7, 6] :id "pawn2"}
+                       piece-4 {:piece :rook :color :black :pos [1, 1] :id "rook1"}
+                       board (-> (s/init-board)
+                                 (s/put-piece-on-board piece-1)
+                                 (s/put-piece-on-board piece-2)
+                                 (s/put-piece-on-board piece-3)
+                                 (s/put-piece-on-board piece-4))
+                       state (->> {:board board :turn :white}
+                                  (g/calculate-all-moves)
+                                  (g/calculate-all-attacks)
+                                  (g/update-check))]
+                   (g/mate? state)))))
+
     (t/testing "white king cornered, mate"
       (t/is (let [piece-1 {:piece :king :color :white :pos [8, 8] :id "king1"}
                   piece-2 {:piece :pawn :color :black :pos [8, 7] :id "pawn1"}
@@ -47,4 +64,3 @@
               (g/mate? state))))))
 
 (t/run-tests)
-
